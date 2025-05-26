89d022405d3f231976ea40bdf3fd427f21ea24c0
Igor Andrade <aigor0863@gmail.com>
2025-05-26 10:33:52 -0300

    segundo commit

--- src/components/Exemplo2.jsx
+++ src/components/Exemplo2.jsx
@@ -5,10 +5,21 @@ import axios from "axios"
 
 export function Exemplo2 () {
     const [cep, setCep] = useState("")
+    const [logradouro,setLogradouro] = useState("")
+    const [bairro,setBairro] = useState("")
+    const [cidade,setCidade] = useState("")
+    const [estado,setEstado] = useState("")
+    const [uf,setUf] = useState("")
 
     useEffect(() => {
         if (cep.length === 8) {
-            axios.get(`https://viacep.com.br/ws/${cep}/json/`).then((Response) => console.log(Response.data))
+            axios.get(`https://viacep.com.br/ws/${cep}/json/`).then((Response) => {
+                setLogradouro(Response.data.logradouro)
+                setBairro(Response.data.bairro)
+                setCidade(Response.data.localidade)
+                setEstado(Response.data.estado)
+                setUf(Response.data.uf)
+            })
         }
     }, [cep])
 
@@ -18,6 +29,45 @@ export function Exemplo2 () {
             <h2>Exemplo 2: Busca CEP</h2>
 
             <input type="number" placeholder="Digte seu CEP" onChange={(input) => setCep(input.target.value)}/>
+
+            {cep.length === 8 && (
+                <>
+                <h3>Suas informacoes</h3>
+                <ul>
+                    <li>Rua: {logradouro}</li>
+                    <li>Bairro: {bairro}</li>
+                    <li>Cidade: {cidade}</li>
+                    <li>Estado: {estado} - {uf}</li>
+                </ul>
+                </>
+            )}
+
+            <form action="">
+                <div>
+                    <label htmlFor="">Rua:</label>
+                    <input type="text" value={logradouro}/>
+                </div>
+
+                <div>
+                    <label htmlFor="">Bairro:</label>
+                    <input type="text" value={bairro}/>
+                </div>
+
+                <div>
+                    <label htmlFor="">Cidade:</label>
+                    <input type="text" value={cidade}/>
+                </div>
+
+                <div>
+                    <label htmlFor="">Estado:</label>
+                    <input type="text" value={estado}/>
+                </div>
+
+                <div>
+                    <label htmlFor="">UF:</label>
+                    <input type="text" value={uf}/>
+                </div>
+            </form>
         </section> 
     )
 }
